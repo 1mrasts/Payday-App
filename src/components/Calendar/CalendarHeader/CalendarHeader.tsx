b45c56c9b1e35c9fe4661092b0833b3efa8e469d
1mrasts <olgaakshidi.ru@gmail.com>
2026-01-25 22:59:22 +0300

    SCRUM-5: fix key error on the map method

--- src/components/Calendar/CalendarHeader/CalendarHeader.tsx
+++ src/components/Calendar/CalendarHeader/CalendarHeader.tsx
@@ -4,8 +4,8 @@ export function CalendarHeader() {
 	const weekDay = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
 	return (
 		<div className={styles['calendar__header']}>
-			{weekDay.map(item => (
-				<span>{item}</span>
+			{weekDay.map((item, index) => (
+				<span key={index}>{item}</span>
 			))}
 		</div>
 	)
