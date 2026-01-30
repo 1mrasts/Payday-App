01f20500a3e5a3120b31915d7de4b4ba686d5f30
1mrasts <olgaakshidi.ru@gmail.com>
2026-01-30 17:59:51 +0300

    SCRUM-8: add salary state to control salary days

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -3,14 +3,17 @@ import { CalendarDays } from './CalendarDays/CalendarDays'
 import type { datesType } from './CalendarDays/CalendarDays.types'
 import { CalendarHeader } from './CalendarHeader/CalendarHeader'
 import { CalendarInfo } from './CalendarInfo/CalendarInfo'
+import { Settings } from './Settings/Settings'
 
 export function Calendar() {
 	const [days, setDays] = useState<datesType[]>([])
+	const [salary, setSalary] = useState<number>(141)
 	return (
 		<main>
 			<CalendarHeader />
-			<CalendarDays days={days} setDays={setDays} />
+			<CalendarDays days={days} setDays={setDays} salary={salary} />
 			<CalendarInfo days={days} />
+			<Settings setSalary={setSalary} />
 		</main>
 	)
 }
