57cf17e9a388b9ac17d6e9b27d19b3c158c5dade
1mrasts <olgaakshidi.ru@gmail.com>
2026-02-09 21:06:04 +0300

    SCRUM-17: style the current calendar day

--- src/components/Calendar/CalendarDay/CalendarDay.tsx
+++ src/components/Calendar/CalendarDay/CalendarDay.tsx
@@ -1,23 +1,38 @@
 import type { datesType } from '../CalendarDays/CalendarDays.types'
+import endDay from '/src/assets/CalendarDay/End-day.svg'
+import moneyWallet from '/src/assets/CalendarDay/Money-wallet.svg'
+import startDay from '/src/assets/CalendarDay/Start-day.svg'
 
 export function CalendarDay({ day }: { day: datesType | undefined }) {
 	if (!day?.meta) return null
 	return (
-		<>
+		<div className='calendar__block'>
 			<h4>{day.date.toLocaleDateString('ru-RU')}</h4>
-			<p>Почасовая ставка: {day.meta?.price ? day.meta.price : 'Неизвестно'}</p>
-			<p>
-				Время начала:{' '}
-				{day.meta?.startTime
-					? day.meta.startTime.toLocaleTimeString()
-					: 'Неизвестно'}
-			</p>
-			<p>
-				Время окончания:{' '}
-				{day.meta?.finishTime
-					? day.meta.finishTime.toLocaleTimeString()
-					: 'Неизвестно'}
-			</p>
-		</>
+			<div className='sep'></div>
+			<div className='calendar__col'>
+				<img src={moneyWallet} alt='' />
+				<p>
+					Почасовая ставка: {day.meta?.price ? day.meta.price : 'Неизвестно'}
+				</p>
+			</div>
+			<div className='calendar__col'>
+				<img src={startDay} alt='' />
+				<p>
+					Время начала:{' '}
+					{day.meta?.startTime
+						? day.meta.startTime.toLocaleTimeString()
+						: 'Неизвестно'}
+				</p>
+			</div>
+			<div className='calendar__col'>
+				<img src={endDay} alt='' />
+				<p>
+					Время окончания:{' '}
+					{day.meta?.finishTime
+						? day.meta.finishTime.toLocaleTimeString()
+						: 'Неизвестно'}
+				</p>
+			</div>
+		</div>
 	)
 }
